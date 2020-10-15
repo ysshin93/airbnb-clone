66aa4b050764314851d7b22a4b60672cb3f42399
ysshin93 <troy.yoonsik.shin@gmail.com>
2020-10-15 19:16:53 -0400

    update

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Home from './Home'
+import Header from './Header'
+
+function App() {
+  return (
+
+    // BEM
+    <div className="app">
+      <Header />
+      <Home />
+      
+      {/*Home */}
+      {/*Header*/}
+
+      {/* Banner */}
+        {/*Search */}
+      {/*Cards */}
+      {/*Footer */}
+    </div>
+    );
+}
+
+export default App;
